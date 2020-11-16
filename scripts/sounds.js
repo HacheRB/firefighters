19c5b1fab35ef8c338f3ec257d43c2fc87b4ed51
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2020-11-16 21:19:56 +0000

    planteamiento sound

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,16 +1,19 @@
 //sounds 
-
-function playSound (sound, vol){
-    let soundSelect = document.getElementById(sound);
-    soundSelect.volume = vol;
-    soundSelect.play() ;
+const AUDIOS = {
+    fire1: new Audio("assets/music/fire1.wav"),
 }
 
-function loopSound (sound, vol){
+function playSound(sound, vol) {
+    let soundSelect = document.getElementById(sound);
+    soundSelect.volume = vol;
+    soundSelect.play();
+}
+
+function loopSound(sound, vol) {
     let soundSelect = document.getElementById(sound);
     //ponerlo con loop
     soundSelect.volume = vol;
-    soundSelect.play() ;
+    soundSelect.play();
 }
 
 //para el fuego utilizar los loop 
